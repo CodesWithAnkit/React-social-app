ea0ef0d7fc3b0d20253de754c1f3d3dbe1e63a07
codeswithankit <ank8804465648@gmail.com>
2020-05-25 15:50:38 +0530

    react-tooltip

--- client/app/components/ViewSinglePost.js
+++ client/app/components/ViewSinglePost.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
 import LoadingDotsIcons from './LoadingDotsIcons'
 import ReactMarkdown from 'react-markdown'
+import ReactTooltip from 'react-tooltip'
 
 const ViewSinglePost = () => {
   const { id } = useParams()
@@ -48,12 +49,23 @@ const ViewSinglePost = () => {
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
         <span className="pt-2">
-          <a href="#" className="text-primary mr-2" title="Edit">
+          <a
+            href="#"
+            data-tip="Edit"
+            data-for="edit"
+            className="text-primary mr-2"
+          >
             <i className="fas fa-edit"></i>
           </a>
-          <a className="delete-post-button text-danger" title="Delete">
+          <ReactTooltip id="edit" className="custom-tooltip" />{' '}
+          <a
+            className="delete-post-button text-danger"
+            data-tip="Delete"
+            data-for="delete"
+          >
             <i className="fas fa-trash"></i>
           </a>
+          <ReactTooltip id="delete" className="custom-tooltip" />
         </span>
       </div>
 
