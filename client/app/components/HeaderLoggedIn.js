258c23347ee1ce7d736fdd6900a3159e7579d2c7
codeswithankit <ank8804465648@gmail.com>
2020-07-07 11:24:25 +0530

    Search Component

--- client/app/components/HeaderLoggedIn.js
+++ client/app/components/HeaderLoggedIn.js
@@ -1,24 +1,33 @@
-import React, { useEffect, useContext } from "react"
-import { Link } from "react-router-dom"
-import DispatchContext from "../DispatchContext"
-import StateContext from "../StateContext"
+import React, { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import DispatchContext from "../DispatchContext";
+import StateContext from "../StateContext";
 
 function HeaderLoggedIn(props) {
-  const appDispatch = useContext(DispatchContext)
-  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
 
-  function handleLogout() {
-    appDispatch({ type: "logout" })
-  }
+  const handleLogout = () => {
+    appDispatch({ type: "logout" });
+  };
+
+  const handleSearchIcon = (e) => {
+    e.preventDefault();
+    appDispatch({ type: "openSearch" });
+  };
 
   return (
     <div className="flex-row my-3 my-md-0">
-      <a href="#" className="text-white mr-2 header-search-icon">
+      <a
+        onClick={handleSearchIcon}
+        href="#"
+        className="text-white mr-2 header-search-icon"
+      >
         <i className="fas fa-search"></i>
       </a>
       <span className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
-        <span className="chat-count-badge text-white"> </span>
+        <span className="chat-count-badge text-white"></span>
       </span>
       <Link to={`/profile/${appState.user.username}`} className="mr-2">
         <img className="small-header-avatar" src={appState.user.avatar} />
@@ -30,7 +39,7 @@ function HeaderLoggedIn(props) {
         Sign Out
       </button>
     </div>
-  )
+  );
 }
 
-export default HeaderLoggedIn
+export default HeaderLoggedIn;
