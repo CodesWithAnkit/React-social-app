e6ec6149215869f043b00d64abdd1e8c8bc007e4
codeswithankit <ank8804465648@gmail.com>
2020-07-08 12:10:26 +0530

    Little component setup

--- client/app/components/Home.js
+++ client/app/components/Home.js
@@ -5,6 +5,7 @@ import { useImmer } from "use-immer";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import { Link } from "react-router-dom";
+import Post from "./Post";
 
 function Home() {
   const appState = useContext(StateContext);
@@ -50,27 +51,7 @@ function Home() {
           </h2>
           <div className="list-group">
             {state.feed.map((post) => {
-              const date = new Date(post.createdDate);
-              const dateFormatted = `${date.getMonth() +
-                1}/${date.getDate()}/${date.getFullYear()}`;
-
-              return (
-                <Link
-                  onClick={() => appDispatch({ type: "closeSearch" })}
-                  key={post._id}
-                  to={`/post/${post._id}`}
-                  className="list-group-item list-group-item-action"
-                >
-                  <img className="avatar-tiny" src={post.author.avatar} />
-                  {" "}
-                  <strong>{post.title}</strong>
-                  {" "}
-                  <span className="text-muted small">
-                    by {post.author.username} on {dateFormatted}
-                    {" "}
-                  </span>
-                </Link>
-              );
+              <Post post={post} key={post._id} />;
             })}
           </div>
         </>
