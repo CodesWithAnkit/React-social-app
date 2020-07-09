49ea645f89954f275124d70d456ba9dd9ba7fe7c
codeswithankit <ank8804465648@gmail.com>
2020-07-09 11:20:24 +0530

    added chat component and used useRef for auto focus the input

--- client/app/components/HeaderLoggedIn.js
+++ client/app/components/HeaderLoggedIn.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom'
-import DispatchContext from '../DispatchContext'
-import StateContext from '../StateContext'
-import ReactTooltip from 'react-tooltip'
+import React, { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import DispatchContext from "../DispatchContext";
+import StateContext from "../StateContext";
+import ReactTooltip from "react-tooltip";
 
 function HeaderLoggedIn(props) {
-  const appDispatch = useContext(DispatchContext)
-  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
 
   const handleLogout = () => {
-    appDispatch({ type: 'logout' })
-  }
+    appDispatch({ type: "logout" });
+  };
 
   const handleSearchIcon = (e) => {
-    e.preventDefault()
-    appDispatch({ type: 'openSearch' })
-  }
+    e.preventDefault();
+    appDispatch({ type: "openSearch" });
+  };
 
   return (
     <div className="flex-row my-3 my-md-0">
@@ -28,16 +28,19 @@ function HeaderLoggedIn(props) {
       >
         <i className="fas fa-search"></i>
       </a>
-      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />{' '}
+      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />
+      {" "}
       <span
         data-for="chat"
         data-tip="Chat"
         className="mr-2 header-chat-icon text-white"
+        onClick={() => appDispatch({ type: "toggleChat" })}
       >
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"></span>
       </span>
-      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />{' '}
+      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />
+      {" "}
       <Link
         data-for="profile"
         data-tip="My-profile"
@@ -46,16 +49,17 @@ function HeaderLoggedIn(props) {
       >
         <img className="small-header-avatar" src={appState.user.avatar} />
       </Link>
-      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />{' '}
+      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />
+      {" "}
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
       </Link>
       <button onClick={handleLogout} className="btn btn-sm btn-secondary">
-        {' '}
+        {" "}
         Sign Out
       </button>
     </div>
-  )
+  );
 }
 
-export default HeaderLoggedIn
+export default HeaderLoggedIn;
